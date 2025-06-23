d01ab6b025720d63f4e86b853e424ebce4dbf0e5
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-06-23 12:28:50 +0200

    script: autostart.sh fix pipewire low volume

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,6 +5,21 @@ echo none > /sys/class/leds/led-0/trigger
 echo none > /sys/class/leds/led-1/trigger
 echo none > /sys/class/leds/led-2/trigger
 
+# wait for kodi to start in background
+(
+  KODI_RUNNING=0
+  while [ $KODI_RUNNING != 1 ]; do
+    KODI_RUNNING=`ps -A | grep kodi.bin | grep -v grep | wc -l`
+    if [ $KODI_RUNNING == 1 ]; then
+      sleep 5
+      # fix pipewire low volume (kodi needs to be running)
+      wpctl set-volume @DEFAULT_AUDIO_SINK@ 100%
+      break
+    fi
+    sleep 5
+  done
+)&
+
 if [ -f $HOME/.config/scripts/prevent_idle.sh ]; then
   (
   sh $HOME/.config/scripts/prevent_idle.sh
