21f0f7f93cc0b7f6a3879f6300a4e10bdf2c0236
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2025-08-27 12:05:20 +0200

    rtcwake.sh: curl waketime

--- scripts/rtcwake.sh
+++ scripts/rtcwake.sh
@@ -1,7 +1,32 @@
 #!/bin/sh
 
+curlwithcode() {
+  local code=0
+  local tmpfile; tmpfile=$(mktemp /tmp/curl.XXXXXX)
+  body="null"
+
+  status_code=$(curl --connect-timeout 3 --retry 0 -s -w "%{http_code}" -o >(cat >"$tmpfile") "$1") || code="$?"
+  [ $status_code = 200 ] && body="$(cat "$tmpfile")"
+  rm "$tmpfile"
+
+  return $code
+}
+
+curlwithcode http://10.0.0.10:1880/wake_time
+
+# could not get wake_time, exit with 1
+[ $body = "null" ] && exit 1
+
+# got off, disable rtc wakealarm
+if [ $body = "off" ]; then
+  echo 0 > /sys/class/rtc/rtc0/wakealarm
+  logger -t rtcwake.sh "rtc: unset wakealarm"
+  exit 0
+fi
+
 # set rtc wakealarm to wake from suspend
-WAKE_AT="08:00"
+#WAKE_AT="08:00"
+WAKE_AT="$body"
 
 if [ -f /sys/class/rtc/rtc0/wakealarm ]; then
   CURRENT_TIME=$(date +%s)
@@ -15,8 +40,8 @@ if [ -f /sys/class/rtc/rtc0/wakealarm ]; then
   # get configured wake time from system
   WAKE_ALARM=$(cat /sys/class/rtc/rtc0/wakealarm)
 
-  # update wakealarm if empty or already passed
-  if [ -z $WAKE_ALARM ] || [ 1$WAKE_ALARM -lt 1$WAKE_TIME ]; then
+  # update wakealarm if empty or different
+  if [ -z $WAKE_ALARM ] || [ 1$WAKE_ALARM != 1$WAKE_TIME ]; then
     echo 0 > /sys/class/rtc/rtc0/wakealarm
     echo $WAKE_TIME > /sys/class/rtc/rtc0/wakealarm
     logger -t rtcwake.sh "rtc: set wakealarm to $(date -d @$WAKE_TIME)"
