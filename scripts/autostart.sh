5282968cf7515e0cd5b245930e95545e6eaab833
s7a7ic <36445466+s7a7ic@users.noreply.github.com>
2026-07-08 17:51:04 +0200

    update autostart.sh

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -8,15 +8,17 @@
 echo none | tee /sys/class/leds/led-?/trigger > /dev/null
 
 # enable bfq scheduler for emmc/sdcard
-echo bfq | tee /sys/block/mmcblk?/queue/scheduler > /dev/null
+#echo bfq | tee /sys/block/mmcblk?/queue/scheduler > /dev/null
 
 # run prevent_idle in background
-if [ -f ${SCRIPTS_PATH:-.}/prevent_idle.sh ]; then
-  sh ${SCRIPTS_PATH:-.}/prevent_idle.sh &
-fi
+[ -f ${SCRIPTS_PATH:-.}/prevent_idle.sh ] && sh ${SCRIPTS_PATH:-.}/prevent_idle.sh &
 
 # run irexec for special keys on ir-remote
 [ -f /storage/.config/lircrc ] && irexec -d /storage/.config/lircrc
 
 wait_for_network
+
+# trigger home assistant update_entity
+curl ${CURL_OPT} -d 'id=media_player.tinkerelec' ${CURL_URL}/update_entity
+
 power_tv "on"
